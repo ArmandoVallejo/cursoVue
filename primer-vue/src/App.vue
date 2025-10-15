9497e400d93644cffcbdedadd41c0872031f6ee3
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-15 16:07:48 -0600

    Se manejan estilos dinamicos con :class

--- primer-vue/src/App.vue
+++ primer-vue/src/App.vue
@@ -1,22 +1,43 @@
 <script setup>
+
+  import {ref} from 'vue';
   const name = "Armando";
 
   //methods
-  const handleClick = (message) => {
-    console.log(message);
+  const counter = ref(0);
 
+  const increment = () =>{
+    counter.value++;
+  }
+
+  const decrement = () =>{
+    counter.value--;
+  }
+
+  const reset = () =>{
+    counter.value = 0;
   }
 
 </script>
 <!--v-on se puede abreviar con @-->
 <template>
-  <button v-on:click.right.prevent="handleClick('boton derecho')">Boton Right</button>
-  <button @click.left="handleClick('boton izquierdo')">Boton Left</button>
-  <button @click.middle="handleClick('boton central')">Boton Middle</button>
+
+  <h2 :class="counter > 0 ? 'positive' : 'negative'">{{ counter }}</h2>
+
+  <button @click="increment">Aumentar Contador</button>
+  <button @click="decrement">Disminuir Contador</button>
+  <button @click="reset">Resetear Contador</button>
 </template>
 
-<style>
+<style> 
   h1{
     color:red;
+  }
+  .positive{
+    color:green
+  }
+
+  .negative{
+    color:red
   }
 </style>
