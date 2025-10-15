a4a0d0117f569ede8eed471e34e5969c95e2aaf6
ArmandoVallejo <juanarmandovh20@gmail.com>
2025-10-15 14:53:11 -0600

    se trabajo con v-for para recorrer objetos y arreglos

--- primer-vue/src/App.vue
+++ primer-vue/src/App.vue
@@ -3,6 +3,29 @@
   const styleColor = "color: blue";
   const arrayColores = ["azul", "verde", "rojo"];
   const activo = true;
+  const arrayFrutas = [
+    {
+      name:"Manzana",
+      price:"$1.00",
+      description:"Una manzana"
+    },
+    {
+      name:"Pera",
+      price:"$2.00",
+      description:"Una pera"
+    },
+    {
+      name:"Naranja",
+      price:"$3.00",
+      description:"Una naranja"
+    }, 
+  ];
+
+  const objetoFruta = {
+    name:"Manzana",
+    price:"$1.00",
+    description:"Una manzana"
+  }
 </script>
 <!-- v-bind puede ser reemplazado dejando solamente los : -->
 <template>
@@ -14,6 +37,18 @@
   <h2 v-else>Estoy inactivo</h2>
 
   <h2 v-show="activo">activo v-show</h2>
+
+  <ul>
+    <li v-for="(fruta) in arrayFrutas" :key="fruta.name">{{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }}
+    </li>
+  </ul>
+
+  <ul>
+    <li
+      v-for="(value, propiedad) in objetoFruta"
+    >
+  {{propiedad}}:{{ value }}</li>
+  </ul>
 
 
 </template>
