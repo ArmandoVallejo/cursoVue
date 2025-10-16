61d42c1457bafce200f9c14bae644d87612c31da
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-16 14:32:44 -0600

    Feat :sparkles: Add the v-for example with a component

--- components-1/src/App.vue
+++ components-1/src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
 import ButtonCounter from './components/ButtonCounter.vue';
 import BlogPost from './components/BlogPost.vue';
+import { ref } from 'vue';
+
+const posts = ref([
+  {title:'Post 1', id: 1, text:'Texto para el componente 1'},
+  {title:'Post 2', id: 2, text:'Texto para el componente 2'},
+  {title:'Post 3', id: 3, text:'Texto para el componente 3'},
+  {title:'Post 4', id: 4, text:'Texto para el componente 4'},
+
+]);
 </script>
 
 <template>
   <h1>App</h1>
   <ButtonCounter />
-  <BlogPost title="Post 01" :id="1" text="Texto para el componente 1" colorText="primary"/>
-  <BlogPost title="Post 02" :id="2" text="Texto para el componente 2" colorText="secondary"/>
-  <BlogPost title="Post 03" :id="3" text="Texto para el componente 3" colorText="success"/>
+  <BlogPost
+    v-for="post in posts"
+    :key="post.id"
+    :title="post.title"
+    :id="post.id"
+    :text="post.text"
+
+  />
+  
 
   
 </template>
