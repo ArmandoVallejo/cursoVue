a9b9741f57fdcdc63018bff1ae22e13d1b339eda
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-16 14:22:20 -0600

    Feat :sparkles: Add the example of how to send and receive props

--- components-1/src/App.vue
+++ components-1/src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
 import ButtonCounter from './components/ButtonCounter.vue';
+import BlogPost from './components/BlogPost.vue';
 </script>
 
 <template>
   <h1>App</h1>
   <ButtonCounter />
+  <BlogPost title="Post 01" :id="1" text="Texto para el componente 1" colorText="primary"/>
+  <BlogPost title="Post 02" :id="2" text="Texto para el componente 2" colorText="secondary"/>
+  <BlogPost title="Post 03" :id="3" text="Texto para el componente 3" colorText="success"/>
+
+  
 </template>
---
+++ components-1/src/components/BlogPost.vue
@@ -0,0 +1,22 @@
+<script setup>
+//recibe los props, puede ser con array o con object
+    //defineProps(['title','id','text'])
+    defineProps({
+        title: String,
+        id: Number,
+        text: {
+            type: String,
+            deafult: 'Sin texto'
+        }
+    })
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{id}}-{{ title }}</h5>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
