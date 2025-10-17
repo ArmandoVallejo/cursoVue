c5a5b2e0ce940ad3f9dd4005d122fd1a54ac6f23
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-17 16:24:42 -0600

    style: :art: Modify the pokemons list design and navbar color

--- router-1/src/views/PokemonsView.vue
+++ router-1/src/views/PokemonsView.vue
@@ -10,10 +10,10 @@
     <p v-if="loading">Cargando...</p>
         <div class="alert alert-danger" v-if="errorData">{{ errorData }}</div>
 
-    <div v-if="data">
+    <div v-if="data" class="mb-5">
         <h1>Pokemons</h1>
-        <ul>
-            <li v-for="pokemon in data.results" :key="pokemon.name">
+        <ul class="list-group mb-3">
+            <li v-for="pokemon in data.results" :key="pokemon.name" class="list-group-item">
                 <router-link :to="`/pokemons/${pokemon.name}`"> {{ pokemon.name }} </router-link>
             </li>
         </ul>
