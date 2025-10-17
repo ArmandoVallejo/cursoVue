4a1a6228e472971251a70f02db730d16a3852698
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-17 13:45:23 -0600

    Feat :sparkles: Get the list of pokemons using axios

--- router-1/src/views/PokemonsView.vue
+++ router-1/src/views/PokemonsView.vue
@@ -1,4 +1,25 @@
+<script setup>
+    import axios from 'axios';
+    import { ref } from 'vue';
 
+    const pokemons = ref([]);
+
+    const getData = async() =>{
+        try {
+            const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+            pokemons.value = data.results;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    getData();
+</script>
 <template>
     <h1>Pokemons</h1>
+    <ul>
+        <li v-for="pokemon in pokemons" :key="pokemon.name">
+            {{ pokemon.name }}
+        </li>
+    </ul>
 </template>
