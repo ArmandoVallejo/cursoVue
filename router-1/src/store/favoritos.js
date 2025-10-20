20b83217ea532aa801833f53aa09409393d64933
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-20 12:37:48 -0600

    feat (localStorage): :sparkles: Add the favorite pokemons to the localStorage

--- router-1/src/store/favoritos.js
+++ router-1/src/store/favoritos.js
@@ -5,12 +5,18 @@ export const useFavoritosStore = defineStore('favoritos', ()=>{
 
     const favoritos = ref([]);
 
+    if(localStorage.getItem('favoritos')){
+        favoritos.value = JSON.parse(localStorage.getItem('favoritos'))
+    }
+
     const addFav = (pokemon) => {
         favoritos.value.push(pokemon)
+        localStorage.setItem('favoritos', JSON.stringify(favoritos.value));
     }
     
     const removeFav = (id) => {
         favoritos.value = favoritos.value.filter(item => item.id !== id)
+        localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
     }
 
     const findPoke = (name) => favoritos.value.find(item => item.name === name);
