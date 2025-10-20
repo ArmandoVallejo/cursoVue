6c6ce96fe0691b3cab783e227c25aef200826855
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-20 11:19:05 -0600

    feat :sparkles: Configure counter store with composition API

--- router-1/src/store/counter.js
+++ router-1/src/store/counter.js
@@ -1,6 +1,18 @@
 import {defineStore} from "pinia";
+import { computed, ref } from "vue";
 
-export const useCounterStore = defineStore('counter',{
+export const useCounterStore = defineStore("counter", () => {
+    const count = ref(0);
+    const increment = () => count.value++
+    const double = computed(()=> count.value * 2)
+    return {
+        count,
+        increment,
+        double
+    };
+});
+
+/*export const useCounterStore = defineStore('counter',{
     state: () => ({
         count: 0
     }),
@@ -12,4 +24,4 @@ export const useCounterStore = defineStore('counter',{
     getters: {
         double: (state) => state.count * 2
     }
-})
+})*/
